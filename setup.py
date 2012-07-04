08ed9be75f39772599e76d31822ce65c94777681
Allis Tauri <allista@gmail.com>
2012-07-04 18:40:16 +0400

    Version change to 1.1.

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def read(fname):
 
 from distutils.core import setup
 setup(name='degen-primer',
-      version='1.0',
+      version='1.1',
       description='Tool to compute degenerate primer parameters using BioPython',
       long_description=read('README'),
       license='GPL-3',
