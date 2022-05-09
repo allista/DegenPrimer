7844e24bb7cb6ad978d90cfe2423f7be6ed7db78
Allis Tauri <allista@gmail.com>
2022-05-09 16:01:54 +0300

    Changed version to 2.9

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ from DegenPrimer.UnifiedNN import UnifiedNN
 
 from distutils.core import setup
 setup(name='degen-primer',
-      version='2.8-rc3',
+      version='2.9',
       description='Degenerate primer characterization and '
                   'in silico PCR simulation software.',
       long_description=read('README.md'),
